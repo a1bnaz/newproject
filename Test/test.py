cd85c0b33db8245284ec33ad723764dab9f8c635
albert <ieatbaconhair@gmail.com>
2024-11-10 23:29:15 -0500

    initial commit

--- Test/test.py
+++ Test/test.py
@@ -1,10 +1,23 @@
-print("hello world")
 
-x = 10
-y = 20
-print(x + y)
+def no_lola(names):
+    if len(names) == 0:
+        return []
+    if names[0] == "Lola":
+        return no_lola(names[1:])
+    else:
+        return [names[0]] + no_lola(names[1:])
+         
 
-def dirty(x,y):
-    return x + y
+def has_balanced_parens(the_string, open_parens=0):
 
-print(dirty(5,10))
+    if len(the_string) == 0:
+        return open_parens == 0
+
+    if the_string[0] == "(":
+        open_parens += 1
+        return has_balanced_parens(the_string[1:], open_parens)
+    elif the_string[0] == ")":
+        open_parens -= 1
+        return has_balanced_parens(the_string[1:], open_parens)
+    else:
+        return has_balanced_parens(the_string[1:], open_parens)
